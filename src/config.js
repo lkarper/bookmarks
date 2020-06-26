2d3e01cb3812992719cd74f5be7511e264d9fb80
Luther Karper <luther.karper@live.com>
2020-06-26 14:54:26 -0400

    Prepare for deployment

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export default {
-  API_ENDPOINT: `http://localhost:8000/api/bookmarks`,
-  API_KEY: 'd548553c-5706-4f3c-be21-6feb14ea197a',
+  API_ENDPOINT: `https://young-castle-86300.herokuapp.com/`,
+  API_KEY: process.env.REACT_APP_API_KEY,
 }
